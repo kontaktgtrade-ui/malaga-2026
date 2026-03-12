823b65a4e73f80558db949fbc15e23b6b4426e6c
kontaktgtrade-ui <kontakt.gtrade@gmail.com>
2026-03-12 14:18:02 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Service Worker dla PWA Malaga 2026
+self.addEventListener('install', () => {
+  console.log('Malaga App Installed');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Wymagane do działania PWA
+});
