7a02aaa366c5947c470006993804064c05e51741
kontaktgtrade-ui <kontakt.gtrade@gmail.com>
2026-03-12 14:26:46 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Rejestracja Service Workera dla trybu Standalone
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js').then(reg => {
+      console.log('SW registered');
+    }).catch(err => {
+      console.log('SW error', err);
+    });
   });
 }
 
